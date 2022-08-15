61cf368a1ee4a40b12a0ec9cef7d62cfb41f736f
andrew <andrew@andrewdeMacBook-Pro.local>
2022-08-15 13:58:04 +0800

    add returns for function of aidrdrop interface

--- contracts/interfaces/IAirdrop.sol
+++ contracts/interfaces/IAirdrop.sol
@@ -17,19 +17,19 @@ interface IAirdrop {
     }
 
     function addActivity() external returns (uint256);
-    function activities(uint256 id) external;
+    function activities(uint256 id) external returns (Activity memory);
     function removeActivity(uint256 id) external;
     function addUserRewards(uint256 id, address user, uint256 targetId, uint256 amounts) external;
     function addUsersRewards(address[] memory users, uint256[] memory targetIds, uint256[] memory amounts) external;
     function removeUserRewards(uint256 id, address user, uint256 amounts) external;
     function removeusersRewards(address[] memory users, uint256[] memory targetIds, uint256[] memory amounts) external;
-    function withdrawRemain(uint256 id) external;
-    function setStatus(uint256 id, bool status) external;
+    function withdrawRemain(uint256 id) external returns (uint256);
+    function openActivity(uint256 id) external;
+    function closeActivity(uint256 id) external;
 
     event AddActivity(uint id);
     event RemoveActivity(uint id);
     event AddUserRewards(uint256 id, address user, uint256 amounts);
     event RemoveUserRewards(uint256 id, address user, uint256 amounts);
-    event WithdrawRemain(uint256 id);
     event SetStatus(uint256 id, bool status);
 }
