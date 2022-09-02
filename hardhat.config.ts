8e2fd991838f72ac6bd3ef8e0512707fe7de4cee
deploy <123456@qq.com>
2022-09-02 02:27:32 +0000

    update config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,61 +1,59 @@
+// require('@nomiclabs/hardhat-waffle');
 import { HardhatUserConfig } from "hardhat/config";
-import { config as dotenvConfig } from "dotenv";
-import "hardhat-abi-exporter";
-import "@nomicfoundation/hardhat-toolbox";
-import { resolve } from "path";
+import "@nomiclabs/hardhat-ethers";
 
-const dotenvConfigPath: string = process.env.DOTENV_CONFIG_PATH || "./.env";
-dotenvConfig({ path: resolve(__dirname, dotenvConfigPath) });
+import { join } from 'path';
+
+import dotenv from 'dotenv';
+const file = join("~", ".env-secret");
+
+const envConfig = dotenv.config({
+  path: file
+}).parsed;
+
+if (!envConfig) {
+  console.error(`open ${file} fail`);
+  process.exit(1);
+}
+
+const privateKey = process.env.PRIVATE_KEY;
+
+if (!privateKey) {
+  console.error('undefined key');
+  process.exit(1);
+}
+
 
 const config: HardhatUserConfig = {
-  solidity: {
-    compilers: [
-      {
-          version: "0.8.14"
-      },
-      {
-          version: "0.8.9"
-      },
-    ],
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
+    solidity: {
+      compilers: [
+        {
+            version: "0.8.14"
+        },
+        {
+            version: "0.8.9"
+        },
+      ],
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
       }
-    }
-  },
-  networks: {
-    mainnet: {
-      url:process.env.BSC_MAINNET_URL,
-      chainId: 56,
-      accounts:[process.env.PRIVATE_KEY || ''],
     },
-    bsctest: { 
-      url:process.env.BSC_TESTNET_ARC_URL,
-      chainId: 97,
-      accounts:[process.env.PRIVATE_KEY || ''],
-    },
-    kovan: {
-      url: process.env.KOVAN_URL,
-      chainId: 42,
-      accounts:[process.env.PRIVATE_KEY || ''],
-    },
-    
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
-};
-
-const abiExporter = {
-  path: './data/abi',
-  runOnCompile: true,
-  clear: true,
-  flat: true,
-  only: [':ERC20$'],
-  spacing: 2,
-  pretty: true,
-  format: "minimal",
-};
+    networks: {
+        main: {
+          url: `http://192.168.0.20:8545`, //<---- YOUR INFURA ID! (or it won't work)
+          accounts: [`${privateKey}`],
+        },
+        testnet: {
+          url: `https://data-seed-prebsc-1.arcdex.io:8575`, //<---- YOUR INFURA ID! (or it won't work)
+          accounts: [`${privateKey}`],
+        },
+      },
+    // etherscan: {
+    //   apiKey: process.env.ETHERSCAN_API_KEY,
+    // },
+  };
 
 export default config;
